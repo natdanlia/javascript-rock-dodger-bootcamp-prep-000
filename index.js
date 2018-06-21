9dd552b32e8d6b7860ec2272e757332dc4401ee2
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-06-21 23:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval);
-  game.removeEventListener('keydown', moveDodger())
+  game.removeEventListener(moveDodger)
   
   
 }
